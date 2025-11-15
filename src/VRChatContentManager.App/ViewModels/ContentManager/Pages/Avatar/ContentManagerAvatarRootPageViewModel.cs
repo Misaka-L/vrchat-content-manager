c1d019f3d61578c66e7d18fd846ec063bfbc9fc7
Misaka-L <lipww1234@foxmail.com>
2025-11-15 15:10:20 +0800

    feat: content management pages (w.i.p)

---
+++ src/VRChatContentManager.App/ViewModels/ContentManager/Pages/Avatar/ContentManagerAvatarRootPageViewModel.cs
@@ -0,0 +1,8 @@
+﻿using VRChatContentManager.App.ViewModels.Pages;
+
+namespace VRChatContentManager.App.ViewModels.ContentManager.Pages.Avatar;
+
+public sealed class ContentManagerAvatarRootPageViewModel : PageViewModelBase
+{
+    
+}
