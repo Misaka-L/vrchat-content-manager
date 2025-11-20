6cee93e310d96e0aa947a670a2bd2f39b5a46863
Misaka-L <lipww1234@foxmail.com>
2025-11-20 19:43:00 +0800

    refactor: split different api category to different files

---
+++ src/Core/VRChatContentManager.Core/Services/VRChatApi/VRChatApiClient.Avatar.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http.Json;
+using VRChatContentManager.Core.Models.VRChatApi;
+using VRChatContentManager.Core.Models.VRChatApi.Rest;
+using VRChatContentManager.Core.Models.VRChatApi.Rest.Avatars;
+
+namespace VRChatContentManager.Core.Services.VRChatApi;
+
+public sealed partial class VRChatApiClient
+{
+    public async ValueTask<VRChatApiAvatar> GetAvatarAsync(string avatarId,
+        CancellationToken cancellationToken = default)
+    {
+        var response = await httpClient.GetAsync($"avatars/{avatarId}", cancellationToken);
+
+        await HandleErrorResponseAsync(response);
+
+        var avatar =
+            await response.Content.ReadFromJsonAsync(ApiJsonContext.Default.VRChatApiAvatar, cancellationToken);
+        if (avatar is null)
+            throw new UnexpectedApiBehaviourException("The API returned a null avatar object.");
+
+        return avatar;
+    }
+
+    public async ValueTask<VRChatApiAvatar> CreateAvatarVersionAsync(string avatarId,
+        CreateAvatarVersionRequest createRequest, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var request = new HttpRequestMessage(HttpMethod.Put, $"avatars/{avatarId}")
+        {
+            Content = JsonContent.Create(createRequest, ApiJsonContext.Default.CreateAvatarVersionRequest)
+        };
+
+        var response = await httpClient.SendAsync(request, cancellationToken);
+
+        await HandleErrorResponseAsync(response);
+
+        var world = await response.Content.ReadFromJsonAsync(ApiJsonContext.Default.VRChatApiAvatar, cancellationToken);
+        if (world is null)
+            throw new UnexpectedApiBehaviourException("The API returned a null avatar object.");
+
+        return world;
+    }
+}
