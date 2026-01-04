82fcde3c9016ca2e289f9f66b4e6da47aa8fe678
Misaka-L <lipww1234@foxmail.com>
2026-01-05 03:48:18 +0800

    feat: third-part notices and acknowledgement in about view

--- src/App/VRChatContentPublisher.App/ViewModels/Settings/AboutSettingsViewModel.cs
+++ src/App/VRChatContentPublisher.App/ViewModels/Settings/AboutSettingsViewModel.cs
@@ -7,4 +7,7 @@ public sealed class AboutSettingsViewModel : ViewModelBase
     public string AppVersion => AppVersionUtils.GetAppVersion();
     public string AppCommitHash => AppVersionUtils.GetAppCommitHash();
     public DateTimeOffset? AppBuildDate => AppVersionUtils.GetAppBuildDate();
+
+    public const string ThirdPartNoticeRelativePath = "THIRD-PARTY-NOTICES.TXT";
+    public string ThirdPartNoticePath => Path.GetFullPath(ThirdPartNoticeRelativePath);
 }
