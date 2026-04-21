99b530c5bed19394a61bf52abf8d64b4dc4c0858
Misaka_L <lipww1234@foxmail.com>
2026-04-22 00:51:27 +0800

    fix: activate window won't restore window state

--- src/App/VRChatContentPublisher.App/Views/MainWindow.axaml.cs
+++ src/App/VRChatContentPublisher.App/Views/MainWindow.axaml.cs
@@ -2,12 +2,15 @@ using System.ComponentModel;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Reactive;
 using VRChatContentPublisher.App.ViewModels;
 
 namespace VRChatContentPublisher.App.Views;
 
 public partial class MainWindow : Window
 {
+    private WindowState _lastStateBeforeMinimized;
+
     public MainWindow()
     {
         InitializeComponent();
@@ -34,6 +37,16 @@ public partial class MainWindow : Window
             Hide();
 #endif
         };
+
+        _lastStateBeforeMinimized = WindowState;
+        this.GetObservable(WindowStateProperty)
+            .Subscribe(new AnonymousObserver<WindowState>(state =>
+            {
+                if (state == WindowState.Minimized)
+                    return;
+
+                _lastStateBeforeMinimized = state;
+            }));
     }
 
     private bool IsBorderlessSupported() => OperatingSystem.IsWindows();
@@ -183,5 +196,7 @@ public partial class MainWindow : Window
     {
         Show();
         Activate();
+
+        WindowState = _lastStateBeforeMinimized;
     }
 }
