91ca0647bc5f425a807e6b220b9a4304122d9335
Misaka_L <lipww1234@foxmail.com>
2026-06-08 23:40:03 +0800

    refactor: update sentrey release naming

--- src/Core/VRChatContentPublisher.Core.Shared/Sentry/Extensions/SentrySdkExtension.cs
+++ src/Core/VRChatContentPublisher.Core.Shared/Sentry/Extensions/SentrySdkExtension.cs
@@ -10,27 +10,32 @@ public static class SentrySdkExtension
         {
             SentrySdk.Init(options =>
             {
-                var distribution =
-#if WINDOWS
-                "win-x64";
-#else
-                    OperatingSystem.IsLinux() ? "linux-x64" : "unknown";
-#endif
-
                 options.Dsn =
                     "https://410cfecb0ce1c5ff78c89f94145b0803@o4511519891914752.ingest.de.sentry.io/4511519908364368";
                 options.IsGlobalModeEnabled = true;
-                options.Distribution = distribution;
+                options.Distribution = GetDistribution();
                 options.CacheDirectoryPath = Path.Combine(AppStorageService.GetTempPath(), "sentry-cache");
                 options.EnableLogs = true;
                 options.AutoSessionTracking = true;
+                options.Release = GetRelease();
 #if DEBUG
-                options.Release = $"dev-{AppVersionUtils.GetAppVersion()}+{AppVersionUtils.GetAppCommitHash()}";
                 options.Environment = "debug";
-#else
-            options.Release = AppVersionUtils.GetAppVersion();
 #endif
             });
         }
+    }
+
+    private static string GetRelease()
+    {
+        return $"content-publisher@{AppVersionUtils.GetAppVersion()}+{AppVersionUtils.GetAppCommitHash()}";
+    }
+
+    private static string GetDistribution()
+    {
+#if WINDOWS
+        return "win-x64";
+#else
+        return OperatingSystem.IsLinux() ? "linux-x64" : "unknown";
+#endif
     }
 }
